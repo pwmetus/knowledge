99d634c602887340aa70e72bbaf82fd9016926a3
Yoo myoung Gyu <pwmetus@gmail.com>
2026-02-23 21:12:36 +0900

    WPF Ioc MarkupExtension 예제 추가

---
+++ programing_codes/csharp/WPF_Ioc_MarkupExtension.cs
@@ -0,0 +1,44 @@
+﻿## DiExtension.cs
+
+namespace MyProject;
+public class DiExtension : MarkupExtension
+{
+    public Type? Type { get; set; }
+
+    public override object? ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (Type == null)
+        { return null; }
+
+        if (!DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+        {
+            return Ioc.Default.GetService(Type) ??
+                throw new InvalidOperationException($"No service of type {Type} registered.");
+        }
+
+        // 디자인 Mode 일 경우 기본 생성
+        try
+        {
+            return Activator.CreateInstance(Type);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
+
+## SomeViewModel
+
+public class SomeViewModel : ObservableObject
+{
+    private string _item;
+    public string Item {
+        get => _item; set => SetProperty(ref _item, value);
+    }
+}
+
+## SomeView
+
+<UserControl xmlns:root="clr-namespace:MyProject"
+            DataContext="{root:Di Type={x:Type vm:SomeViewModel}}">
